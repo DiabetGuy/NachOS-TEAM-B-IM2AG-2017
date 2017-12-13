910310a7dcc6903e329fc35fb9f11c65e5923f75
Arnaudouais <arnaud-av@live.fr>
2017-12-13 14:23:44 +0100

    step2 partie2 2

---
+++ code/userprog/synchconsole.cc
@@ -0,0 +1,50 @@
+
+#include "copyright.h"
+#include "system.h"
+#include "synchconsole.h"
+
+static Semaphore *readAvail;
+static Semaphore *writeDone;
+
+static void ReadAvail(int arg) { readAvail->V(); }
+static void WriteDone(int arg) { writeDone->V(); }
+
+SynchConsole::SynchConsole(char *readFile, char *writeFile)
+{
+	readAvail = new Semaphore("read avail", 0);
+	writeDone = new Semaphore("write done", 0);
+	console = new Console(readFile, writeFile, ReadAvail, WriteDone, 0);
+}
+
+SynchConsole::~SynchConsole()
+{
+	delete console;
+	delete writeDone;
+	delete readAvail;
+}
+
+void SynchConsole::SynchPutChar(const char ch)
+{
+	console->PutChar(ch);
+	writeDone->P();
+}
+
+char SynchConsole::SynchGetChar()
+{
+	readAvail->P();
+	return console->GetChar();
+}
+
+void SynchConsole::SynchPutString(const char s[])
+{
+	for (char* s2 = (char*) s; s2[0] != '\0'; s2 = &s2[1]) {
+		this->SynchPutChar(s2[0]); //is s2[0] the same as s2 ?
+	}
+}
+
+void SynchConsole::SynchGetString(char *s, int n)
+{
+	for (int i = 0; i > n; i++) {
+		s[i] = this->SynchGetChar();
+	}
+}
