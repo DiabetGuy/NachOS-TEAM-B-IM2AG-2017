ace83f9da5922b498c943cdb9b196298831d1a04
Amaury Huot <huotam@F202-06.e-im2ag.ujf-grenoble.fr>
2017-12-19 13:18:59 +0100

    Etape 3 partie 1-5(middle)

--- code/test/makethreads.c
+++ code/test/makethreads.c
@@ -1,19 +1,15 @@
 #include "syscall.h"
 
 void thread_loop(void *arg){
-	int i = 0;
-	for(i=0; i<5; i++){
-		PutInt((int)arg);
-	}
+	PutChar('P');
+	PutChar('D');
 }
 
 int main()
 {
-	int un = 1;
 	// int deux = 2;
-	UserThreadCreate(thread_loop, (void*)(&un));
+	UserThreadCreate(thread_loop, 0);
 	//UserThreadCreate(thread_loop, (void*)(&deux));
-
 	PutInt(3);
 
 	//Halt();
--- code/userprog/userthread.cc
+++ code/userprog/userthread.cc
@@ -7,17 +7,37 @@ go there
 
 Thread * newthread;
 
-// static void StartUserThread(int f)
-// {
-// }
+class fa
+{
+public:
+  int f;
+  int arg;
+  fa(int _f, int _arg)
+  {
+     f = _f;
+     arg = _arg;
+  }
+};
+
+static void StartUserThread(int f)
+{
+  machine->WriteRegister(4, ((fa*) f)->arg);
+  machine->WriteRegister(PCReg, ((fa*) f)->f);
+  machine->WriteRegister(NextPCReg, ((fa*) f)->f+4);
+
+  machine->Run();
+}
+
 int do_UserThreadCreate(int f, int arg)
 {
   bool ok = true;
+  fa *f1 = new fa(f, arg);
+
   if(ok)
   {
     newthread = new Thread("usethr");
-    newthread->Fork((VoidFunctionPtr)f,(int)arg );
-    machine->WriteRegister(4, arg);
+    newthread->Fork(StartUserThread,(int) f1 );
+
     // StartUserThread(f);
     return 0;
   }
