5526b9fba41a14dab60d96c91d342f849ae7115a
Arnaudouais <arnaud-av@live.fr>
2018-01-09 14:45:24 +0100

    modif mineure

--- code/userprog/userthread.cc
+++ code/userprog/userthread.cc
@@ -7,13 +7,13 @@ go there
 
 Thread * newthread;
 
-class fa
+class thread_args
 {
 public:
   int f;
   int arg;
   int stackNum;
-  fa(int _f, int _arg, int sn)
+  thread_args(int _f, int _arg, int sn)
   {
      f = _f;
      arg = _arg;
@@ -24,10 +24,10 @@ public:
 static void StartUserThread(int f)
 {
   int sp;
-  machine->WriteRegister(4, ((fa*) f)->arg);
-  machine->WriteRegister(PCReg, ((fa*) f)->f);
-  machine->WriteRegister(NextPCReg, ((fa*) f)->f+4);
-  sp = currentThread->space->stackBeginning - currentThread->space->getSingleStackSize() * ((fa*) f)->stackNum;
+  machine->WriteRegister(4, ((thread_args*) f)->arg);
+  machine->WriteRegister(PCReg, ((thread_args*) f)->f);
+  machine->WriteRegister(NextPCReg, ((thread_args*) f)->f+4);
+  sp = currentThread->space->stackBeginning - currentThread->space->getSingleStackSize() * ((thread_args*) f)->stackNum;
   machine->WriteRegister(StackReg, sp);
 
   machine->Run();
@@ -45,7 +45,7 @@ int do_UserThreadCreate(int f, int arg)
       return -1;
     }
 
-    fa *f1 = new fa(f, arg, stackNum);
+    thread_args *f1 = new thread_args(f, arg, stackNum);
 
     newthread = new Thread("usethr");
     newthread->stackNum = stackNum;
