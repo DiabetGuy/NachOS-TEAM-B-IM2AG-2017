9a298066a499111647fac94ebf35c665e244886e
hoangtha <momojean0@gmail.com>
2018-01-09 16:07:33 +0100

    Etape 4 Partie 1 Question 5 finie

--- code/userprog/frameprovider.cc
+++ code/userprog/frameprovider.cc
@@ -1,13 +1,32 @@
 #include "copyright.h"
 #include "utility.h"
+#include "bitmap.h"
 
-
-FrameProvider::FrameProvider()
+FrameProvider::FrameProvider(int NumPhysPages, int PageSize, char* mainMemory)
 {
-
+  framemap=new BitMap(NumPhysPages);
+  physicalPageSize=PageSize;
+  memory=mainMemory;
 }
 
 FrameProvider::~FrameProvider()
 {
-  
+  delete framemap;
+}
+
+int FrameProvider::GetEmptyFrame()
+{
+  int frameNb = framesMap->Find();
+  bzero(memory + frameNb * physPageSize, physPageSize);
+  return frameNb;
+}
+
+void FrameProvider::ReleaseFrame()
+{
+  framesMap->Clear(frameNb);
+}
+
+int FrameProvider::NumAvailFrame()
+{
+    return framesMap->NumClear();
 }
