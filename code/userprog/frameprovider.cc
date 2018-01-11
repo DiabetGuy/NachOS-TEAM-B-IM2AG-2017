a99cf903b0d7d499fbb228b0f85a466fe5c29880
Amaury Huot <huotam@F214-13.e-im2ag.ujf-grenoble.fr>
2018-01-11 10:45:23 +0100

    Etape 4 partie 1 question 6 aléatoire finie

--- code/userprog/frameprovider.cc
+++ code/userprog/frameprovider.cc
@@ -18,23 +18,21 @@ int FrameProvider::GetEmptyFrame()
 {
   int frameNb = framemap->Find();
   bzero(memory + frameNb * physicalPageSize, physicalPageSize);
+
   return frameNb;
 }
 
 int FrameProvider::GetEmptyFrameRandom()
 {
-  int nbClear = framemap->NumClear ();
-  int i, j=0;
-	int frameNb = 0;
+  srand(time(NULL));
   int nbFrames = framemap->GetSize();
-	srand(time(NULL)); // initialisation de rand
-	frameNb = rand()%(nbClear) +1;
-  for(i=0; i<nbFrames || j == frameNb; i++){
-    if (!framemap->Test (i)) j++;
-  }
-  bzero(memory + i * physicalPageSize, physicalPageSize);
-  framemap->Mark(i);
-  return i;
+  int res = rand()%(nbFrames-1);
+  while(framemap->Test(res))
+  {
+		res=rand()%(nbFrames-1);
+	}
+  framemap->Mark(res);
+  return res;
 }
 
 void FrameProvider::ReleaseFrame(int frameNb)
